3c0a0d111ea23b0e96defc96d523de297de9d0be
benjamin farmer <viroben2001@gmail.com>
2022-10-03 19:24:10 -0700

    new final edit

--- index.js
+++ index.js
@@ -8,9 +8,35 @@ function newImage(url, left, bottom){
     return object
 }
 
-function newItem(url, left, bottom){
+
+function newItem(url, left, bottom) {
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function () {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
+
+function newInventory() {
+    let           = document.createElement('div')
+    inventory.style.position = 'fixed'
+    inventory.style.bottom = '0px';
+    inventory.style.left = '0px'
+    inventory.style.width = '100%'
+    inventory.style.height = '100px'
+    inventory.style.display = 'flex'
+    inventory.style.flexDirection = 'row'
+    inventory.style.alignItems = 'center'
+    inventory.style.justifyContent = 'space-evenly'
+    inventory.style.border = '2px solid black'
+    inventory.style.backgroundColor = 'brown'
+    document.body.append(inventory)
+}
+
+
+
 
 function newInventory(){
     let inventory = document.createElement('div')
